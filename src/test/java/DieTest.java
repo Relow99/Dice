eed2c13df8acda018055e4850c1735e73368f842
Tshwarelo <59955727+Relow99@users.noreply.github.com>
2020-08-03 14:05:47 +0200

    added tests

--- src/test/java/DieTest.java
+++ src/test/java/DieTest.java
@@ -1,14 +1,50 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DieTest {
-    @Test
-    void setProbabilities() {
+public class DieTest {
+        public int value;
+        Random r = new Random();
+        private int sides;
+        private int [] probabilities;
+        double random = Math.random();
 
-    }
+        @Test
+   public void setProbabilities(int[] probabilities) {
+            this.probabilities = this.probabilities;
+            sides = 6;
+            this.probabilities = new int[]{6};
+            int result = 0;
+            for (int probability : this.probabilities) result += probability;
+
+            double [] np = new double[result];
+            if(this.probabilities.length ==sides){
+                for (int i = 0; i < this.probabilities.length ; i++) {
+                    np[i] = (this.probabilities[i])/(result*1.0);
+                }
+                for (int i = 0; i < this.probabilities.length ; i++) {
+                    if (random< np[i]) {
+                        value = i+1;
+                        break;
+                    }
+                }
+            }
+            Assert.assertEquals(6, result);
+        }
 
     @Test
-    void roll() {
+  public void roll() {
+        sides = 6;
+        value = r.nextInt(sides )+1;
+        try {
+            setProbabilities(probabilities);
+        }catch (NullPointerException e){
+
+            Assert.assertFalse(value ==0);
+        }
+
     }
 }
